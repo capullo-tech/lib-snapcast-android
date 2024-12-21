94e767f012537874857c422b824e512ddd4aa76d
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2024-12-21 17:01:32 +0800

    initial commit

--- lib-snapcast-android/build.gradle.kts
+++ lib-snapcast-android/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.android.build.gradle.internal.registerDependencyCheck
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -14,7 +17,9 @@ android {
         consumerProguardFiles("consumer-rules.pro")
         externalNativeBuild {
             cmake {
-                cppFlags("")
+                arguments += listOf("-DANDROID_STL=c++_static", "-DBUILD_SERVER=ON", "-DBUILD_TESTS=OFF")
+                cppFlags += listOf("-std=c++14")
+                abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
             }
         }
     }
@@ -30,7 +35,7 @@ android {
     }
     externalNativeBuild {
         cmake {
-            path("src/main/cpp/CMakeLists.txt")
+            path("src/main/cpp/snapcast/CMakeLists.txt")
             version = "3.22.1"
         }
     }
@@ -41,6 +46,15 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        prefab = true
+    }
+    ndkVersion = "25.1.8937393"
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
 }
 
 dependencies {
@@ -48,7 +62,29 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("com.github.badaix:oboe:1.9.0@aar")
+    implementation("com.github.badaix:boost:1.85.0@aar")
+    implementation("com.github.badaix:flac:1.4.2@aar")
+    implementation("com.github.badaix:ogg:1.3.5@aar")
+    implementation("com.github.badaix:opus:1.1.2@aar")
+    implementation("com.github.badaix:soxr:0.1.3@aar")
+    implementation("com.github.badaix:tremor:1.0.1@aar")
+    implementation("com.github.badaix:vorbis:1.3.7@aar")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "tech.capullo"
+            artifactId = "lib-snapcast-android"
+            version = "0.1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
