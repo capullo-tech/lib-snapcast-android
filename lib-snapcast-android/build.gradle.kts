15455480695a58b31579ae96992a7619ba1a7b24
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2024-12-22 14:06:51 +0800

    Add the GitHub package registry reference to lib-snapcast-android module. Removing the git submodule initialize from jitpack.yml

--- lib-snapcast-android/build.gradle.kts
+++ lib-snapcast-android/build.gradle.kts
@@ -78,4 +78,14 @@ publishing {
             }
         }
     }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/capullo-tech/lib-snapcast-android")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
